05b3ff6f9367ae42068c79d1f858bf67b63f4c4f
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-25 11:08:11 -0800

    required necessary modules  and defined middleware

--- server/server.js
+++ server/server.js
@@ -3,8 +3,23 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//integrating updated middleware
+//requiring necessary modules
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware, // Use the updated auth middleware
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
