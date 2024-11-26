40361509981678066b622439f63b32d487d2851e
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-25 16:25:00 -0800

    added code to make every request work with apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,47 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+//taken from module 21, activity 24
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// taken from module 21, student activity 24
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
       <Navbar />
       <Outlet />
-    </>
+      </div>
+    </ApolloProvider>
   );
 }
 
