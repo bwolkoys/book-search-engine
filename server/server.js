cebcd85e0e7b1651d0859374be900d6320318e5d
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-02 13:51:42 -0800

    added code to server.js

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
 
 
 
